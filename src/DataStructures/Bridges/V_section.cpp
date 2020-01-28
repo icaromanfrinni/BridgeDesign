f8b319259154e5efd8747c794e7c5205acf93fdc
icaromanfrinni <icarosousa@gmail.com>
2020-01-27 22:36:12 -0300

    extrude and sweep

--- src/DataStructures/Bridges/V_section.cpp
+++ src/DataStructures/Bridges/V_section.cpp
@@ -62,75 +62,7 @@ void V_section::update()
 	EulerOp::mev(model->halfEdges[0], NULL, 1, newVertex);
 	// f1
 	EulerOp::mef(model->halfEdges[0], model->halfEdges[3], 0);
-	// "U" SECTION
-	// v3
-	newVertex = start_point - (vUp * TOP_LAYER);
-	EulerOp::mvfs(model, newVertex);
-	model->name = "V_section";
-	// v1
-	newVertex = model->vertices.back()->point - (vRight * B / 2.0f) - (vUp * B / 2.0f * SLOPE);
-	EulerOp::mev(model->faces[0]->hEdge, NULL, 0, newVertex);
-	// v2
-	newVertex = model->vertices.back()->point + (vRight * B);
-	EulerOp::mev(model->halfEdges[0], NULL, 1, newVertex);
-	// f1
-	EulerOp::mef(model->halfEdges[0], model->halfEdges[3], 0);
-
-	// TRIANGULATED FACE (GL_TRIANGLES)
-	// --------------------------------
-	/*
-	// FIRST CROSS SECTION
-	// v0
-	newVertex = start_point - (vUp * TOP_LAYER);
-	EulerOp::mvfs(model, newVertex);
-	model->name = "V_section";
-	// v1
-	newVertex = model->vertices.back()->point - (vRight * B / 2.0f) - (vUp * B / 2.0f * SLOPE);
-	EulerOp::mev(model->faces[0]->hEdge, NULL, 0, newVertex);
-	// v2, f0
-	newVertex = model->vertices.back()->point + (vRight * (B - b) / 2.0f) - (vUp * h);
-	EulerOp::mev(model->halfEdges[0], NULL, 1, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[0], 0);
-	// v3, f1
-	newVertex = model->vertices[2]->point - (vUp * (H - h));
-	EulerOp::mev(model->halfEdges[3], NULL, 1, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[3], 1);
-	// v4, f2
-	newVertex = model->vertices[1]->point + (vRight * B / 2.0f) - (vUp * (H - h));
-	EulerOp::mev(model->halfEdges[8], NULL, 3, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[8], 2);
-	// v5, f3
-	newVertex = model->vertices[3]->point + (vRight * b);
-	EulerOp::mev(model->halfEdges[11], NULL, 3, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[11], 3);
-	// v6, f4
-	newVertex = model->vertices[2]->point + (vRight * b);
-	EulerOp::mev(model->halfEdges[16], NULL, 5, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[16], 4);
-	// v7, f5
-	newVertex = model->vertices[1]->point + (vRight * B);
-	EulerOp::mev(model->halfEdges[19], NULL, 5, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[19], 5);
-	// f6
-	EulerOp::mef(model->halfEdges[24], model->halfEdges[12], 6);
-	// f7
-	EulerOp::mef(model->halfEdges[24], model->halfEdges[4], 7);
-	// f8
-	EulerOp::mef(model->halfEdges[23], model->halfEdges[28], 8);
-
-	// EXTRUDE
-	// v8, f9
-	newVertex = model->vertices[0]->point + (direction * L);
-	EulerOp::mev(model->halfEdges[1], NULL, 0, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[1], 9);
-	// v9, f10
-	newVertex = model->vertices[1]->point + (direction * L);
-	EulerOp::mev(model->halfEdges.back()->opp, NULL, 8, newVertex);
-	EulerOp::mef(model->halfEdges.back(), model->halfEdges[34], 10);
-	// v10, f11
-	newVertex = model->vertices[2]->point + (direction * L);
-	EulerOp::mev(model->halfEdges.back()->opp, NULL, 9, newVertex);
-	EulerOp::mef(model->halfEdges[33], model->halfEdges.back(), 11);
-	*/
 	
+	// "U" SECTION
+		
 }
--- src/DataStructures/EulerOp.h
+++ src/DataStructures/EulerOp.h
@@ -5,7 +5,7 @@
 namespace EulerOp
 {
 	//MAKE Vertex Face Solid
-	void mvfs(HED::solid* &model, const CRAB::Vector4Df& point)
+	void mvfs(std::vector<HED::solid*>& model, const CRAB::Vector4Df& point)
 	{
 		//Initialize
 		HED::solid* newSolid = new HED::solid;
@@ -14,7 +14,7 @@ namespace EulerOp
 		HED::halfEdge* newHE = new HED::halfEdge;
 
 		//Solid
-		newSolid->id = 0;
+		newSolid->id = model.size();
 
 		//Vertex
 		newVertex->id = 0;
@@ -37,7 +37,7 @@ namespace EulerOp
 		newHE->vStart = newVertex;
 
 		//RETURN
-		model = newSolid;
+		model.push_back(newSolid);
 	}
 
 	//MAKE Edge Vertex
@@ -63,14 +63,14 @@ namespace EulerOp
 		newHE1->leftFace = he1->leftFace;
 		newHE1->vStart = currentSolid->vertices[v];
 		newHE2->vStart = currentSolid->vertices.back();
+		newHE1->opp = newHE2;
+		newHE2->opp = newHE1;
 
 		if (he2 == NULL)
 		{
 			newHE2->leftFace = he1->leftFace;
 			newHE1->next = newHE2;
-			newHE1->opp = newHE2;
 			newHE2->prev = newHE1;
-			newHE2->opp = newHE1;
 
 			if (currentSolid->halfEdges.empty())
 			{
@@ -90,15 +90,15 @@ namespace EulerOp
 		{
 			newHE2->leftFace = he2->leftFace;
 
-			std::cout << "entrou" << std::endl;
+			//std::cout << "entrou" << std::endl;
 
 			for (HED::halfEdge* he = he1; he != he2; he = he->opp->next)
 				he->vStart = currentSolid->vertices.back();
 
-			std::cout << "meio" << std::endl;
+			//std::cout << "meio" << std::endl;
 
 			//adjacencies
-			newHE1->next = he1;
+			/*newHE1->next = he1;
 			newHE1->prev = he1->prev;
 			he1->prev->next = newHE1;
 			he1->prev = newHE1;
@@ -106,9 +106,17 @@ namespace EulerOp
 			newHE2->next = he2;
 			newHE2->prev = he2->prev;
 			he2->prev->next = newHE2;
+			he2->prev = newHE2;*/
+			newHE1->next = he1;
+			newHE1->prev = he1->prev;
+			he1->prev->next = newHE1;
+			he1->prev = newHE1;
+			newHE2->next = he2;
+			newHE2->prev = he2->prev;
+			he2->prev->next = newHE2;
 			he2->prev = newHE2;
 
-			std::cout << "saiu" << std::endl;
+			//std::cout << "saiu" << std::endl;
 		}
 
 		//RETURN
@@ -173,8 +181,260 @@ namespace EulerOp
 			currentSolid->faces[f]->hEdge = he2;
 		}
 
+		/* CONDIÇÃO DE ORIENTAÇÃO NÃO FUNCIONANDO */
+		/*
+		//Face
+		//ok
+		newFace->id = currentSolid->faces.size();
+		newFace->HedSolid = currentSolid;
+		//check the loop orientation
+		if (he2->vStart == currentSolid->faces[f]->hEdge->vStart)
+		{
+			//nok
+			newFace->hEdge = he1;
+			//nok
+			for (HED::halfEdge* he = he1; he != he2; he = he->next)
+				he->leftFace = newFace; //currentSolid->faces.back();
+		}
+		else
+		{
+			newFace->hEdge = he2;
+			for (HED::halfEdge* he = he2; he != he1; he = he->next)
+				he->leftFace = newFace;
+		}
+		//ok
+		currentSolid->faces.push_back(newFace);
+
+		//Half-Edge
+		//ok
+		newHE1->id = currentSolid->halfEdges.size();
+		newHE2->id = currentSolid->halfEdges.size() + 1;
+		newHE1->leftFace = currentSolid->faces.back();
+		newHE2->leftFace = currentSolid->faces[f];
+		newHE1->opp = newHE2;
+		newHE2->opp = newHE1;
+		//check the loop orientation
+		if (he2->vStart == currentSolid->faces[f]->hEdge->vStart)
+		{
+			//nok
+			newHE1->vStart = he2->vStart;
+			newHE2->vStart = he1->vStart;
+			newHE1->next = he1;
+			newHE2->next = he2;
+			newHE1->prev = he2->prev;
+			newHE2->prev = he1->prev;
+			//novas adjacências das half-edges existentes
+			//nok
+			he1->prev->next = newHE2;
+			he2->prev->next = newHE1;
+			he1->prev = newHE1;
+			he2->prev = newHE2;
+			//nova half-edge de referência da face existente
+			//nok
+			currentSolid->faces[f]->hEdge = he2;
+		}
+		else
+		{
+			newHE1->vStart = he1->vStart;
+			newHE2->vStart = he2->vStart;
+			newHE1->next = he2;
+			newHE2->next = he1;
+			newHE1->prev = he1->prev;
+			newHE2->prev = he2->prev;
+			//novas adjacências das half-edges existentes
+			he1->prev->next = newHE1;
+			he2->prev->next = newHE2;
+			he1->prev = newHE2;
+			he2->prev = newHE1;
+			//nova half-edge de referência da face existente
+			//nok
+			currentSolid->faces[f]->hEdge = he1;
+		}
+		*/
+
 		//RETURN
 		currentSolid->halfEdges.push_back(newHE1);
 		currentSolid->halfEdges.push_back(newHE2);
+	}
+
+	// MODIFY
+	// ------
+
+	// Translational Sweep
+	// ID da superfície de base = 1 (face q permanece onde foi criada)
+	// ID da superfície q acompanha a construção do volume = 0 (cópia da face base com deslocamento no sentido do vetor 'dir', sem alterar o sentido da normal)
+	void EXTRUDE(HED::face* f, const CRAB::Vector4Df& dir, const float& depth)
+	{
+		// Get Solid
+		HED::solid* currentSolid = f->HedSolid;
+		HED::halfEdge* he = f->hEdge;
+
+		if (currentSolid->faces.size() == 2)
+		{
+			// first edge
+			CRAB::Vector4Df newVertex = he->vStart->point + dir * depth;
+			mev(he->opp, NULL, he->vStart->id, newVertex);
+
+			// edges and faces
+			for (he = f->hEdge->prev; he != f->hEdge; he = he->prev)
+			{
+				newVertex = he->vStart->point + dir * depth;
+				mev(currentSolid->halfEdges.back()->prev, NULL, currentSolid->vertices.back()->id, newVertex);
+				mef(currentSolid->halfEdges.back(), he->prev->opp, he->prev->opp->leftFace->id);
+			}
+
+			// last face
+			mef(currentSolid->halfEdges.back(), currentSolid->halfEdges.back()->next->next->next, currentSolid->halfEdges.back()->next->next->next->leftFace->id);
+		}
+		else if (currentSolid->faces.size() > 2)
+		{
+			// first edge
+			CRAB::Vector4Df newVertex = he->vStart->point + dir * depth;
+			mev(he->prev->opp, he->opp->next, he->vStart->id, newVertex);
+			// the other edges
+			for (he = f->hEdge->next; he != f->hEdge; he = he->next)
+			{
+				newVertex = he->vStart->point + dir * depth;
+				mev(he->prev->opp, he->opp->next, he->vStart->id, newVertex);
+			}
+			// close the first new_loop (new face)
+			mef(he->opp->prev, he->opp->next->next, he->opp->leftFace->id);
+			// the other loops
+			for (he = f->hEdge->next; he != f->hEdge; he = he->next)
+				mef(he->opp->prev, he->opp->next->next, he->opp->leftFace->id);
+		}
+	}
+
+	// Translational Sweep
+	// ID da superfície de base = 1 (face q permanece onde foi criada)
+	// ID da superfície q acompanha a construção do volume = 0 (cópia da face base com deslocamento no sentido do vetor 'dir')
+	// Vetor normal da nova face na mesma direção do deslocamento 'dir'
+	// Distância dos novos vértices é calculada em função do ponto de referência (vStart da halfEdge de referência da face base)
+	void NORMAL_EXTRUDE(HED::face* f, const CRAB::Vector4Df& dir, const float& depth)
+	{
+		// Get Solid
+		HED::solid* currentSolid = f->HedSolid;
+		HED::halfEdge* he = f->hEdge;
+		CRAB::Vector4Df refPoint = he->vStart->point;
+
+		if (currentSolid->faces.size() == 2)
+		{
+			// new vertex
+			CRAB::Vector4Df newVertex = refPoint + dir * depth;
+			// first edge
+			mev(he->opp, NULL, he->vStart->id, newVertex);
+			// edges and faces
+			for (he = f->hEdge->prev; he != f->hEdge; he = he->prev)
+			{
+				// new vertex
+				CRAB::Vector4Df refP_p = he->vStart->point - refPoint;
+				CRAB::Vector4Df normal = cross(refP_p, dir).to_unitary();
+				CRAB::Vector4Df new_dir = cross(dir, normal).to_unitary();
+				newVertex = currentSolid->vertices.back()->point + new_dir * (refP_p.length());
+				// update parameters
+				refPoint = he->vStart->point;
+				// new edge
+				mev(currentSolid->halfEdges.back()->prev, NULL, currentSolid->vertices.back()->id, newVertex);
+				// new face
+				mef(currentSolid->halfEdges.back(), he->prev->opp, he->prev->opp->leftFace->id);
+			}
+			// last face
+			mef(currentSolid->halfEdges.back(), currentSolid->halfEdges.back()->next->next->next, 0);
+		}
+		else if (currentSolid->faces.size() > 2)
+		{
+			// first edge
+			CRAB::Vector4Df newVertex = refPoint + dir * depth;
+			mev(he->prev->opp, he->opp->next, he->vStart->id, newVertex);
+			// the other edges
+			for (he = f->hEdge->next; he != f->hEdge; he = he->next)
+			{
+				// new vertex
+				CRAB::Vector4Df refP_p = he->vStart->point - refPoint;
+				CRAB::Vector4Df normal = cross(refP_p, dir).to_unitary();
+				CRAB::Vector4Df new_dir = cross(dir, normal).to_unitary();
+				newVertex = currentSolid->vertices.back()->point + new_dir * (refP_p.length());
+
+				// update parameters
+				refPoint = he->vStart->point;
+
+				// new edge
+				//mev(he->opp, NULL, he->vStart->id, newVertex);
+				mev(he->prev->opp, he->opp->next, he->vStart->id, newVertex);
+			}
+			// close the first new_loop (new face)
+			mef(he->opp->prev, he->opp->next->next, he->opp->leftFace->id);
+			// the other loops
+			for (he = f->hEdge->next; he != f->hEdge; he = he->next)
+				mef(he->opp->prev, he->opp->next->next, he->opp->leftFace->id);
+		}
+	}
+
+	// SWEEP: the profile is aligned to be normal to the tangent direction of the sweep path
+	// f = face q sofre a extrusão
+	// tan_dir = direção tangente no novo ponto do alinhamento (perpendicular à nova face)
+	// depth = distância entre o ponto de referência e o novo ponto
+	void SWEEP(HED::face* f, const CRAB::Vector4Df& tan_dir, const float& depth)
+	{
+		// Get Solid
+		HED::solid* currentSolid = f->HedSolid;
+		HED::halfEdge* he = f->hEdge;
+		CRAB::Vector4Df refPoint = he->vStart->point;
+
+		if (currentSolid->faces.size() == 2)
+		{
+			// new vertex
+			CRAB::Vector4Df n = cross(he->next->vStart->point - he->vStart->point, he->next->next->vStart->point - he->next->vStart->point).to_unitary();
+			CRAB::Vector4Df d = (n * (-1.0f) + tan_dir).to_unitary();
+			CRAB::Vector4Df newVertex = refPoint + d * depth;
+			// first edge
+			mev(he->opp, NULL, he->vStart->id, newVertex);
+			// edges and faces
+			for (he = f->hEdge->prev; he != f->hEdge; he = he->prev)
+			{
+				// new vertex
+				CRAB::Vector4Df refP_p = he->vStart->point - refPoint;
+				CRAB::Vector4Df normal = cross(refP_p, d).to_unitary();
+				CRAB::Vector4Df new_dir = cross(tan_dir, normal).to_unitary();
+				newVertex = currentSolid->vertices.back()->point + new_dir * (refP_p.length());
+				// update parameters
+				refPoint = he->vStart->point;
+				// new edge
+				mev(currentSolid->halfEdges.back()->prev, NULL, currentSolid->vertices.back()->id, newVertex);
+				// new face
+				mef(currentSolid->halfEdges.back(), he->prev->opp, he->prev->opp->leftFace->id);
+			}
+			// last face
+			mef(currentSolid->halfEdges.back(), currentSolid->halfEdges.back()->next->next->next, 0);
+		}
+		else if (currentSolid->faces.size() > 2)
+		{
+			// first edge
+			CRAB::Vector4Df n = cross(he->next->vStart->point - he->vStart->point, he->next->next->vStart->point - he->next->vStart->point).to_unitary();
+			CRAB::Vector4Df d = (n + tan_dir).to_unitary();
+			CRAB::Vector4Df newVertex = refPoint + d * depth;
+			mev(he->prev->opp, he->opp->next, he->vStart->id, newVertex);
+			// the other edges
+			for (he = f->hEdge->next; he != f->hEdge; he = he->next)
+			{
+				// new vertex
+				CRAB::Vector4Df refP_p = he->vStart->point - refPoint;
+				CRAB::Vector4Df normal = cross(refP_p, d).to_unitary();
+				CRAB::Vector4Df new_dir = cross(tan_dir, normal).to_unitary();
+				newVertex = currentSolid->vertices.back()->point + new_dir * (refP_p.length());
+
+				// update parameters
+				refPoint = he->vStart->point;
+
+				// new edge
+				//mev(he->opp, NULL, he->vStart->id, newVertex);
+				mev(he->prev->opp, he->opp->next, he->vStart->id, newVertex);
+			}
+			// close the first new_loop (new face)
+			mef(he->opp->prev, he->opp->next->next, he->opp->leftFace->id);
+			// the other loops
+			for (he = f->hEdge->next; he != f->hEdge; he = he->next)
+				mef(he->opp->prev, he->opp->next->next, he->opp->leftFace->id);
+		}
 	}
 }
